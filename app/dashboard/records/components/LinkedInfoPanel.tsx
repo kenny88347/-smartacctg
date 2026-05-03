df795d190e8c243cc17be2cd092df76ce8f3d147
kenny88347 <77531618+kenny88347@users.noreply.github.com>
2026-05-03 23:45:13 +0800

    Add LinkedInfoPanel component with LinkedDataForm

---
+++ app/dashboard/records/components/LinkedInfoPanel.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import LinkedDataForm from "../LinkedDataForm";
+
+export function LinkedInfoPanel(props: any) {
+  return <LinkedDataForm {...props} />;
+}
+
+export default LinkedInfoPanel;
